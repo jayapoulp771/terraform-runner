fc56e5812e22e47135ded3c7dc846d9381733335
jayapoulp771 <paulpj771@gmail.com>
2025-11-05 23:51:36 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,12 +1,27 @@
 variable "vpc_id" {
     type = string
-    default = "vpc-vpc-08cca60e36dd91851"
+    default = "vpc-0daa19e29dc8d4dc6"
 }
 variable "subnet_id" {
     type = string
-    default = "subnet-056f1a1d22a52302c"
+    default = "subnet-07558176df797e557"
 }
 variable "ami_id" {
     type = string
     default = "ami-0601422bf6afa8ac3"
+}
+variable "vpc_security_group_id"{
+type = string
+default = "sg-007107407378b9669"  
+}
+
+variable "db_security_group_id" {
+    type = string
+    default = "sg-09b342ca2b5a37b1f"
+  
+}
+variable "route_table_id" {
+    type = string
+    default = "rtb-01436ce21e0e9175d"
+  
 }
