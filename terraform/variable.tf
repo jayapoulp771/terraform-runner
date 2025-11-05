05486eec5828068ae73fe202425f190c7f0c7b03
jayapoulp771 <paulpj771@gmail.com>
2025-11-05 23:49:24 +0530

    Add files via upload

---
+++ terraform/variable.tf
@@ -0,0 +1,12 @@
+variable "vpc_id" {
+    type = string
+    default = "vpc-vpc-08cca60e36dd91851"
+}
+variable "subnet_id" {
+    type = string
+    default = "subnet-056f1a1d22a52302c"
+}
+variable "ami_id" {
+    type = string
+    default = "ami-0601422bf6afa8ac3"
+}
