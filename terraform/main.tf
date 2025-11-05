1167d6e279eed9e8c8bf0fdc3f1a42fda02d4934
jayapoulp771 <paulpj771@gmail.com>
2025-11-05 23:46:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,10 @@
-variable "rhel_ami" {
-    default = "ami-06d2e6fdb95d0813e"
-}
-
-resource "aws_instance" "rhel_mytask" {
-    ami = var.rhel_ami
+    resource "aws_instance" "rhel_mytask" {
+    ami = var.ami_id
     instance_type = "t3.micro"
+    subnet_id = var.subnet_id
     associate_public_ip_address = true
+    vpc_security_group_ids = [aws_security_group.terraform_sg.id]
+    iam_instance_profile = "terraform-runner"
 
     user_data = <<-EOF
     #!/bin/bash
@@ -18,4 +17,12 @@ resource "aws_instance" "rhel_mytask" {
     docker run -d --name mysql-light -e MYSQL_ROOT_PASSWORD=root123 -e MYSQL_DATABASE=mydb -e MYSQL_USER=myuser -e MYSQL_PASSWORD=1234 -p 3306:3306 mysql:8.0
     EOF
 
+    tags = {
+      Name = "rhel_mytask"
+    }
+    }
+
+    resource "aws_route_tale_association" "rtb_assoc" {
+        subnet_id = var.subnet_id
+        route_table_id = var.route_table_id
     }
